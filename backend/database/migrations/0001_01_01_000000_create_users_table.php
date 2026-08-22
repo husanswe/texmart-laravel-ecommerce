1cb7cb59875ff8a8187e855b9d623d6e7ac53350
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-22 15:24:59 +0500

    rewrote the columns in users table

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,17 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone_number')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at');
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('is_admin')->default(false);
+            $table->string('birth_date');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -37,9 +38,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('users');
