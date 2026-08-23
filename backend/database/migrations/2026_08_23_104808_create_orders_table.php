1ae642520158e8f816806c11d2fd8f8789b50bb0
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-23 17:30:03 +0500

    created orders and order_items tables with necessary fields and relationships

---
+++ backend/database/migrations/2026_08_23_104808_create_orders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('order_number')->unique();
+            $table->integer('total_amount');
+            $table->string('status')->default('pending');
+            $table->string('payment_status')->default('unpaid');
+            $table->string('shipping_address');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
