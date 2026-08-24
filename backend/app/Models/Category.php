d81fd5251430ca61602a26de9fd743d7f31a818a
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 13:18:32 +0500

    defined relationship between category, product and attribute models

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -4,10 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'name',
+        'icon',
+        'slug',
+        'parent_id'
+    ];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function product(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function attribute(): BelongsToMany
+    {
+        return $this->belongsToMany(Attribute::class);
+    }
 }
