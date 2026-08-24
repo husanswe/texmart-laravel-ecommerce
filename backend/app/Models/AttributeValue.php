78f0eaa2de2ac3977c6bce72b908f588272de469
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 10:32:09 +0500

    feat: implement AttributeValue model with relationships and fillable attributes; update Product model to include attributeValue relationship

--- backend/app/Models/AttributeValue.php
+++ backend/app/Models/AttributeValue.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class AttributeValue extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'attribute_id',
+        'value'
+    ];
+
+    public function attribute(): BelongsTo
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    public function product(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,12 +2,52 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'description',
+        'brand_id',
+        'slug'
+    ];
+
+    public function favorite(): HasMany 
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function productVariant(): HasMany
+    {
+        return $this->hasMany(ProductVariant::class);
+    }
+
+    public function productImage(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function attributeValue(): BelongsToMany
+    {
+        return $this->belongsToMany(AttributeValue::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
