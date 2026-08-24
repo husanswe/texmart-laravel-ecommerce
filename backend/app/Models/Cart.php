ac35f7ede3dcbfc225d952c6e8dfa293d3bf1bc9
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 08:37:01 +0500

    add CartItem model and establish relationships with Cart and ProductVariant; remove obsolete Cart_item model

--- backend/app/Models/Cart.php
+++ backend/app/Models/Cart.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
@@ -18,5 +19,10 @@ class Cart extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function cartItems(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
     }
 }
