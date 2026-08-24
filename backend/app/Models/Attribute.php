a4adb2a9296e8eaafb1700818fa689d9747547d1
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 16:48:00 +0500

    defined fillable attributes and relationships in Attribute model

--- backend/app/Models/Attribute.php
+++ backend/app/Models/Attribute.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Attribute extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'name',
+        'slug',
+        'unit'
+    ];
+
+    public function attributeValue(): HasMany
+    {
+        return $this->hasMany(AttributeValue::class);
+    }
+
+    public function category(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
