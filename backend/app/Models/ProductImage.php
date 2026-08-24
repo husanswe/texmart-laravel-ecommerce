41c54d7da4ccc0087e843ffde14ea844b2fa3ccb
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 12:51:15 +0500

    defined relationship between product and ProductImage model

--- backend/app/Models/ProductImage.php
+++ backend/app/Models/ProductImage.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductImage extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'product_id',
+        'image_path',
+        'is_primary' => 'boolean',
+        'sort_order' => 'integer'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
