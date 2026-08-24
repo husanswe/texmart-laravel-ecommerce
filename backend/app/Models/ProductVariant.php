d1993cea97fd135dc679ad51decd0ca7559803d7
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 12:15:34 +0500

    feat: define fillable attributes and relationships in ProductVariant model

--- backend/app/Models/ProductVariant.php
+++ backend/app/Models/ProductVariant.php
@@ -4,10 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProductVariant extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'product_id',
+        'name',
+        'price',
+        'sku',
+        'stock'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function orderItem(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function cartItem():HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
