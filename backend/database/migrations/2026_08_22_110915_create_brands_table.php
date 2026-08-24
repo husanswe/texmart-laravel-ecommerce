8e2239eb1d6c7ce4f155c4b10ae8f5c433e697a4
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-24 16:32:59 +0500

    feat: define fillable attributes and relationships in Brand model; create brands table with necessary fields

--- backend/database/migrations/2026_08_22_110915_create_brands_table.php
+++ backend/database/migrations/2026_08_22_110915_create_brands_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->string('logo_path');
             $table->timestamps();
         });
