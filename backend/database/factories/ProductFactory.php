46dd404a860245757eabf710a4c9d96d21819e0c
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-25 19:35:55 +0500

    filled product factory file w/ fake data

--- backend/database/factories/ProductFactory.php
+++ backend/database/factories/ProductFactory.php
@@ -4,19 +4,18 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'price' => fake()->randomFloat(100000, 10000000),
+            'description' => fake()->paragraph(2),
+            'slug' => Str::slug('name'),
         ];
     }
 }
