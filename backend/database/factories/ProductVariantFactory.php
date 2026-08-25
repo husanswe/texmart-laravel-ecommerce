e1b1db9bc7a159157096399760e460cb1a7d0616
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-25 19:41:02 +0500

    removed unnecessary commented code in factory files

--- backend/database/factories/ProductVariantFactory.php
+++ backend/database/factories/ProductVariantFactory.php
@@ -8,15 +8,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductVariantFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            
         ];
     }
 }
