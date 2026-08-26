2239b851d22fdfb8b0dea64628fc64b2e924dd02
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-26 08:21:30 +0500

    Refactored AttributeFactory and cleaned up CategoryFactory by adding attribute definitions and removing commented code

--- backend/database/factories/AttributeFactory.php
+++ backend/database/factories/AttributeFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Attribute;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 
 class AttributeFactory extends Factory
@@ -11,7 +12,9 @@ class AttributeFactory extends Factory
     public function definition(): array
     {
         return [
-            
+            'name' => fake()->name(),
+            'slug' => Str::slug('name'),
+            'unit' => fake()->optional()->randomElement(['GB', 'inch', 'MHz', 'mAh', 'MP', 'kg', 'L', 'W'])
         ];
     }
 }
