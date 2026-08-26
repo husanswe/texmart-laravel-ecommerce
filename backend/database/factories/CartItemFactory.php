3ede03a66567f08bfc6675cd6c9c72d3b79c3055
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-26 16:32:41 +0500

    Refactor CartItemFactory by removing unnecessary comments and updating model references

--- backend/database/factories/CartItemFactory.php
+++ backend/database/factories/CartItemFactory.php
@@ -2,21 +2,19 @@
 
 namespace Database\Factories;
 
-use App\Models\CartItem;
+use App\Models\Cart;
+use App\Models\ProductVariant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 
 class CartItemFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'cart_id' => Cart::factory(),
+            'product_variant_id' => ProductVariant::factory(),
+            'quantity' => fake()->randomDigitNotZero(),
         ];
     }
 }
