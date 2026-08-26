35f7ee993648d411b11e7759386d948fbf175613
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-26 16:20:32 +0500

    Refactor CartFactory by removing unnecessary comments and simplifying the definition method

--- backend/database/factories/CartFactory.php
+++ backend/database/factories/CartFactory.php
@@ -3,20 +3,17 @@
 namespace Database\Factories;
 
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 
 class CartFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'session_id' => null
         ];
     }
 }
