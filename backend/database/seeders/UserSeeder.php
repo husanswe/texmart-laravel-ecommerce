8f6ff88e4d2c56280510db939de8ea88885986a2
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-31 11:12:04 +0500

    Refactor CategorySeeder and UserSeeder by removing unused imports and implementing user creation logic

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,13 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'phone_number' => '+998' . fake()->randomElement(['90', '91','93','94','95','97','99','88','33']) . fake()->numerify('#######'),
+            'email' => 'husanswe1@gmail.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true 
+        ]);
+
+        User::factory()->count(4)->create([
+            'is_admin' => false
+        ]);
     }
 }
