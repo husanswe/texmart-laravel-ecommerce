6ecde97d70da7ff86917a15d7f39b8cca9bf6f5c
husanswe <sulaymonovhusan2002@gmail.com>
2026-09-11 07:07:27 +0500

    Add stock column to product_variants table migration

--- backend/database/migrations/2026_08_22_114208_create_product_variants_table.php
+++ backend/database/migrations/2026_08_22_114208_create_product_variants_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('price');
             $table->string('sku')->unique();
+            $table->string('stock')->default(0);
             $table->timestamps();
         });
     }
