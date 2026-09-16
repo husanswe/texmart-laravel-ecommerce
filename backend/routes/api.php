799af46deeb92981abe6b885e3b49d0a9a75301e
husanswe <sulaymonovhusan2002@gmail.com>
2026-09-16 21:20:50 +0500

    Add missing API controller imports in routes file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\SearchController;
+use App\Http\Controllers\Api\FilterController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\FavoriteController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
