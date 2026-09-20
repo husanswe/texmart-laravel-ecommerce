ec23bcdda61b86819bc0200e3bb1c222d0bed246
husanswe <sulaymonovhusan2002@gmail.com>
2026-09-20 22:46:36 +0500

    Implement index method to retrieve parent categories in CategoryController

--- backend/app/Http/Controllers/Api/CategoryController.php
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        //
+        $ParentCategories = Category::whereNull('parent_id')->get();
     }
 
 
