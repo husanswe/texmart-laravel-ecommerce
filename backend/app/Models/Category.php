55c0011817be7aa29512b1654ce21465549f4a51
husanswe <sulaymonovhusan2002@gmail.com>
2026-09-22 13:03:08 +0500

    Add CategoryResource class for category data transformation

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -18,7 +18,7 @@ class Category extends Model
         'slug',
         'parent_id'
     ];
-
+    
     public function parent(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'parent_id');
