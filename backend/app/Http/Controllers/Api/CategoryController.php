6244835d0e71a912939323c3c3313e0935913d3e
husanswe <sulaymonovhusan2002@gmail.com>
2026-09-22 22:57:57 +0500

    Refactor index method in CategoryController to use CategoryResource for data transformation

--- backend/app/Http/Controllers/Api/CategoryController.php
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
             ->with('children')
             ->get();
 
-        return response()->json(Category::whereNull('parent_id')->with('children')->get());
+        return CategoryResource::collection($parentCategories);
     }
 
 
