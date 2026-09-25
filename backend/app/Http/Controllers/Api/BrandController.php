b9aa8678b2f25525be536bc7387e09297dd0e096
husanswe <sulaymonovhusan2002@gmail.com>
2026-09-25 17:45:51 +0500

    Implement BrandResource and update index method in BrandController to return a collection of brands

--- backend/app/Http/Controllers/Api/BrandController.php
+++ backend/app/Http/Controllers/Api/BrandController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
 
 class BrandController extends Controller
 {
     public function index()
     {
-        //
+        $brands = Brand::select('id', 'name', 'slug', 'logo_path')->orderBy('name')->get();
+
+        return BrandResource::collection($brands);
     }
 
 
